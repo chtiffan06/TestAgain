c0db35a39ffc53d9fe87a649a967c4552a02c94b
chtiffan06 <chtiffan@sheridancollege.ca>
2023-03-05 03:51:12 -0500

    First commit

--- SYST17796_ProjectStarterCode_Group1/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Player.java
+++ SYST17796_ProjectStarterCode_Group1/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Player.java
@@ -10,6 +10,7 @@ package ca.sheridancollege.project;
  *
  * @author dancye
  * @author Paul Bonenfant Jan 2020
+ * @author Tiffany Chan, Faith Salcedo, Xiaoying Wang March 2023
  */
 public abstract class Player {
 
